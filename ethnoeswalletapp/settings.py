759a07a337d9efa227f3e462ea590078ff2d0c8b
ayeni olakunle <ayeniolakunle98@gmail.com>
2025-02-11 22:27:24 +0100

    update 1

--- ethnoeswalletapp/settings.py
+++ ethnoeswalletapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    "rest_framework"
+    "rest_framework",
+    "django_vercel"
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_vercel.middleware.VercelMiddleware"
 ]
 
 REST_FRAMEWORK = {
--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Transaction(models.Model):
-    name = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
